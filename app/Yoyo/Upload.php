08d7bf90acc44ea188d9b6a8742bc72d160efe40
Alejandro S <alejandro@jreviews.com>
2020-10-07 11:40:18 -0400

    image validation for upload component

--- app/Yoyo/Upload.php
+++ app/Yoyo/Upload.php
@@ -6,10 +6,19 @@ use Clickfwd\Yoyo\Component;
 
 class Upload extends Component
 {
+    protected $error;
+
     protected function getImageProperty()
     {
         if (empty($_FILES['photo']['tmp_name'])) {
             return '';
+        }
+
+        if (! $this->isImage($_FILES['photo']))
+        {
+            $this->error = 'Not a valid image';
+
+            return;
         }
 
         $tmp = $_FILES['photo']['tmp_name'];
@@ -17,5 +26,26 @@ class Upload extends Component
         $str = file_get_contents($tmp);
 
         return base64_encode($str);
+    }
+
+    protected function getErrorProperty()
+    {
+        return $this->error;
+    }
+
+    protected function isImage($file)
+    {
+        $whitelist_type = ['image/jpeg', 'image/png','image/gif'];
+        
+        $error = null;
+
+        $fileinfo = finfo_open(FILEINFO_MIME_TYPE);
+    
+        if (in_array(finfo_file($fileinfo, $file['tmp_name']), $whitelist_type)) 
+        {
+            return true;
+        }
+
+        return false;
     }
 }
--- app/resources/views/yoyo/upload.php
+++ app/resources/views/yoyo/upload.php
@@ -1,4 +1,4 @@
-<form class="flex flex-col space-y-4 w-72">  
+<form class="flex flex-col space-y-4">  
 
     <div class="mt-6">
         <label for="photo" class="sr-only">
@@ -33,5 +33,23 @@
                 <?php endif; ?>
             </div>
         </div>
-    </div>       
+    </div>  
+                    
+    <?php if ($this->error): ?>
+    <div class="rounded-md bg-red-50 p-4">
+        <div class="flex">
+            <div class="flex-shrink-0">
+                <svg class="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+                </svg>
+                </div>
+                <div class="ml-3">
+                <h3 class="text-sm leading-5 font-medium text-red-800">
+                    <?php echo $this->error; ?>
+                </h3>
+            </div>
+        </div>
+    </div>
+    <?php endif; ?>
+    
 </form>
